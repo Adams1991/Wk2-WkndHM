20935efbcc8567879d8747c7ae97da8613314d44
Adams1991 <adams121@hotmail.co.uk>
2018-06-01 18:27:55 +0100

    bar_spec setup complete. constant error as expected. unsure of hash config but will check if needed

---
+++ specs/bar_spec.rb
@@ -0,0 +1,26 @@
+require("minitest/autorun")
+require_relative("../drinks.rb")
+require_relative("../bar.rb")
+require_relative("../rooms.rb")
+require_relative("../guests.rb")
+
+class BarTest < MiniTest::Test
+
+  def setup
+    @bar = Bar.new(
+      {
+      :drink_name => "Vodka Shot",
+      :drink_price => 3.00
+      },
+      500)
+  end
+
+  def test_drinks_available
+    assert_equal({:drink_name => "Vodka", :drink_price=> 3.00}, @bar.drinks_available())
+  end
+
+  def test_till_ammount
+    assert_equal(500, @bar.test_till_ammount())
+  end
+
+end
