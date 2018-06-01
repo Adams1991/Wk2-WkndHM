c377d7aeb84db35ad98747257967805fbedc0ad8
Adams1991 <adams121@hotmail.co.uk>
2018-06-01 10:45:08 +0100

    songs_spec setup and returning constant error as expected. Song class creation next

---
+++ specs/songs_spec.rb
@@ -0,0 +1,14 @@
+require("minitest/autorun")
+require_relative("../songs.rb")
+
+class SongsTest < MiniTest::Test
+
+  def setup
+    @song = Song.new("Prizefighter")
+  end
+
+  def test_song_name
+    assert_equal("Prizefighter", @song.song_name())
+  end
+
+end
