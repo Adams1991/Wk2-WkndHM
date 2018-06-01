29f842a5bc04b8ef3531ccad506d0530972ced77
Adams1991 <adams121@hotmail.co.uk>
2018-06-01 14:45:21 +0100

    ignore last commit as alter back to previous working

--- guests.rb
+++ guests.rb
@@ -1,15 +1,12 @@
 class Guest
 
-attr_reader(:guest_name, :fav_song, :guest_money)
+attr_reader(:guest_name, :fav_song)
+attr_accessor(:guest_money)
 
   def initialize(guest_name, guest_money, fav_song)
     @guest_name = guest_name
     @guest_money = guest_money
     @fav_song = fav_song
-  end
-
-  def pay_money
-    @guest_money
   end
 
 
